4d42a174c5d00b4c9ff6bd7b1e0296520c2b9b1a
xuan1992 <wux1992@126.com>
2018-05-15 22:15:11 -0700

    It is a test file!

--- src/dailyAlgorithm/Test.java
+++ src/dailyAlgorithm/Test.java
@@ -1,6 +1,9 @@
 package dailyAlgorithm;
 
 public class Test {
+    public void print() {
+        System.out.println("It is a simple test!");
+    }
     public static void main(String[] args) {
         Test t = new Test();
     }
